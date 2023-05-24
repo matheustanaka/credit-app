df012e58247aa7478b39c80399b81255e4ca7ce5
Matheus Tanaka <matheustanakanog@gmail.com>
2023-05-24 20:19:25 -0300

    Building controller layer

---
+++ src/main/kotlin/me/dio/creditapplicationsystem/dto/CustomeUpdateDto.kt
@@ -0,0 +1,22 @@
+package me.dio.creditapplicationsystem.dto
+
+import me.dio.creditapplicationsystem.entity.Customer
+import java.math.BigDecimal
+
+data class CustomeUpdateDto(
+    val firstName: String,
+    val lastName: String,
+    val income: BigDecimal,
+    val zipCode: String,
+    val street: String,
+) {
+    fun toEntity(customer: Customer): Customer {
+        customer.firstName = this.firstName
+        customer.lastName = this.lastName
+        customer.income = this.income
+        customer.address.zipCode = this.zipCode
+        customer.address.street = this.street
+
+        return customer
+    }
+}
---
+++ src/main/kotlin/me/dio/creditapplicationsystem/dto/CustomerView.kt
@@ -0,0 +1,24 @@
+package me.dio.creditapplicationsystem.dto
+
+import me.dio.creditapplicationsystem.entity.Customer
+import java.math.BigDecimal
+
+data class CustomerView(
+    val firstName: String,
+    val lastName: String,
+    val cpf: String,
+    val email: String,
+    val income: BigDecimal,
+    val zipCode: String,
+    val street: String
+) {
+    constructor(customer: Customer): this (
+        firstName = customer.firstName,
+        lastName = customer.lastName,
+        email = customer.email,
+        cpf = customer.cpf,
+        income = customer.income,
+        zipCode = customer.address.zipCode,
+        street = customer.address.street
+    )
+}
