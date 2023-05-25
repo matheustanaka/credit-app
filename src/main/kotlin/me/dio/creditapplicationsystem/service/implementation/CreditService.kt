4042f1a60b76c45ad63a22077c11033fe764cfdb
Matheus Tanaka <matheustanakanog@gmail.com>
2023-05-24 23:44:36 -0300

    Implement function to solve the challenge about the first day of installment

--- src/main/kotlin/me/dio/creditapplicationsystem/service/implementation/CreditService.kt
+++ src/main/kotlin/me/dio/creditapplicationsystem/service/implementation/CreditService.kt
@@ -1,16 +1,19 @@
 package me.dio.creditapplicationsystem.service.implementation
 
 import me.dio.creditapplicationsystem.entity.Credit
+import me.dio.creditapplicationsystem.exception.BusinessException
 import me.dio.creditapplicationsystem.repository.CreditRepository
 import me.dio.creditapplicationsystem.service.ICreditService
 import org.springframework.stereotype.Service
 import java.lang.RuntimeException
+import java.time.LocalDate
 import java.util.*
 
 @Service
 class CreditService(private val creditRepository: CreditRepository, private val customerService: CustomerService): ICreditService {
     override fun save(credit: Credit): Credit {
         credit.apply { customer = customerService.findById(credit.customer?.id!!) }
+        validateFirstInstallmentDate(credit.dayFirstInstallment)
         return this.creditRepository.save(credit)
     }
 
@@ -19,8 +22,15 @@ class CreditService(private val creditRepository: CreditRepository, private val 
     }
 
     override fun findByCreditCode(customerId: Long, creditCode: UUID): Credit {
-        val credit: Credit = (this.creditRepository.findByCreditCode(creditCode) ?: throw RuntimeException("CreditCode $creditCode not found"))
-        return if(credit.customer?.id == customerId) credit else throw RuntimeException("Contact admin")
+        val credit: Credit = (this.creditRepository.findByCreditCode(creditCode) ?: throw BusinessException("It was not possible to find the CreditCode $creditCode"))
+        return if(credit.customer?.id == customerId) credit else throw IllegalArgumentException("Please, contact application admin")
+    }
+
+    private fun validateFirstInstallmentDate(firstInstallmentDate: LocalDate) {
+        val threeMonthsFromNow = LocalDate.now().plusMonths(3)
+        if (firstInstallmentDate.isAfter(threeMonthsFromNow)) {
+            throw BusinessException("Installment date should be 3 months after the current day.")
+        }
     }
 
 }
