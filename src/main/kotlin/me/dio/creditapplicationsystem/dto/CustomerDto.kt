b83f54c265bc0087accbf3317d9edbb5bc814fa5
Matheus Tanaka <matheustanakanog@gmail.com>
2023-05-24 23:41:37 -0300

    Implement spring validation

--- src/main/kotlin/me/dio/creditapplicationsystem/dto/CustomerDto.kt
+++ src/main/kotlin/me/dio/creditapplicationsystem/dto/CustomerDto.kt
@@ -1,18 +1,23 @@
 package me.dio.creditapplicationsystem.dto
 
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotEmpty
+import jakarta.validation.constraints.NotNull
 import me.dio.creditapplicationsystem.entity.Address
 import me.dio.creditapplicationsystem.entity.Customer
+import org.hibernate.validator.constraints.br.CPF
 import java.math.BigDecimal
 
 data class CustomerDto(
-    val firstName: String,
-    val lastName: String,
-    val cpf: String,
-    val income: BigDecimal,
-    val email: String,
-    val password: String,
-    val zipCode: String,
-    val street: String,
+    @field:NotEmpty(message = "Invalid Input") val firstName: String,
+    @field:NotEmpty(message = "Invalid Input") val lastName: String,
+    @field:NotEmpty(message = "Invalid Input") @CPF(message = "This invalid CPF") val cpf: String,
+    @field:NotNull(message = "Invalid Input") val income: BigDecimal,
+    @field:Email(message = "Invalid email")
+    @field:NotEmpty(message = "Invalid Input") val email: String,
+    @field:NotEmpty(message = "Invalid Input") val password: String,
+    @field:NotEmpty(message = "Invalid Input") val zipCode: String,
+    @field:NotEmpty(message = "Invalid Input") val street: String,
     ) {
 
     fun toEntity(): Customer = Customer(
